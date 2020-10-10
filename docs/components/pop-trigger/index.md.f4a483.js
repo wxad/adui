383921f3ac7a86c92187b8164d62012740248878
yijiejiang <yijiejiang@tencent.com>
2020-10-10 18:26:09 +0800

    update

---
+++ docs/components/pop-trigger/index.md.f4a483.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[38],{1543:function(e,n){e.exports={content:["article",["p","按钮用于开始一个即时操作。"],["h2","何时使用"],["p","标记了一个（或封装一组）操作命令，响应用户点击行为，触发相应的业务逻辑。"],["h2","API"],["p","按钮的属性说明如下："]],meta:{category:"Components",type:"General",title:null,subtitle:"空",filename:"components/pop-trigger/index.md"}}}}]);
