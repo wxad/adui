7dbcd082913d64034e9dd7ce66c1282bd292bbd7
jiangyijie27 <jiangyijie27@users.noreply.github.com>
2020-12-21 11:10:13 +0000

    deploy: aa5741eadee6833288d70ecef22a7cabe3d43425

---
+++ components/card/index.md.140877.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{1323:function(e,n){e.exports={content:["article",["p","卡片是包含一类特定数据集的有层级的模块，用以承载相关联内容。页面设计中，通常以内容决定卡片的布局方式。"],["h2","Props"],["h3","Card"],["h3","Card.Header"]],meta:{category:"Components",type:"General",title:"Card",subtitle:"卡片",filename:"components/card/index.md"}}}}]);
