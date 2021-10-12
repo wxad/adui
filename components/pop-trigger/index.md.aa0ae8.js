5250c08e976de6084d62dd13f2e41e1b0508f227
jiangyijie27 <jiangyijie27@users.noreply.github.com>
2021-10-12 10:24:25 +0000

    deploy: 97906253767d97f4c0add68044b46566332fa10a

--- components/pop-trigger/index.md.aa0ae8.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[40],{1393:function(e,n){e.exports={content:["article",["p","按钮用于开始一个即时操作。"],["h2","何时使用"],["p","标记了一个（或封装一组）操作命令，响应用户点击行为，触发相应的业务逻辑。"],["h2","API"],["p","按钮的属性说明如下："]],meta:{category:"Components",type:"General",title:null,subtitle:"空",filename:"components/pop-trigger/index.md"}}}}]);
