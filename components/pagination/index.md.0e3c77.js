7e527aadc345f838b5fc09c0b6b4c0b0c461b730
jiangyijie27 <jiangyijie27@users.noreply.github.com>
2022-01-27 07:59:15 +0000

    deploy: fc7fc4a34e67f60ee96c3b295e7d244ef72e83d7

--- components/pagination/index.md.0e3c77.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[39],{1408:function(n,e){n.exports={content:["article",["p","分页器用于将大量内容以分页形式间隔，切换选择查看。"],["h2","Props"],["h3","Pagination"]],meta:{category:"Components",type:"General",title:"Pagination",subtitle:"分页器",filename:"components/pagination/index.md"}}}}]);
