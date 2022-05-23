9b315d8f723e7777d168438d854d5e57de130055
jiangyijie27 <jiangyijie27@users.noreply.github.com>
2022-05-23 13:55:39 +0000

    deploy: c71b01f46fb278e85f2cb220e633233356f0131b

---
+++ components/grid/index.md.e7717c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[25],{1396:function(e,o){e.exports={content:["article",["p","栅格用于便捷搭建结构化的页面布局，常用于有明确功能分区的页面，如 Dashboard、项目卡片等。"],["h2","Props"],["h3","Grid.Row"],["h3","Grid.Col"]],meta:{category:"Components",type:"General",title:"Grid",subtitle:"栅格",filename:"components/grid/index.md"}}}}]);
