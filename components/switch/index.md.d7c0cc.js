93ba5c1c22035214522a68824feb34e17db828be
jiangyijie27 <jiangyijie27@users.noreply.github.com>
2022-07-12 05:19:46 +0000

    deploy: 956113aadf135d3ac4c3367445209b884b64e72b

--- components/switch/index.md.d7c0cc.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[58],{1430:function(e,t){e.exports={content:["article",["p","开关用于表示内容的开启与关闭，常用于开启或关闭某一功能、模式，也可以表示业务中的一些特殊逻辑，如广告的“投放/暂停”。"],["h2","Props"],["h3","Switch"]],meta:{category:"Components",type:"General",title:"Switch",subtitle:"开关",filename:"components/switch/index.md"}}}}]);
