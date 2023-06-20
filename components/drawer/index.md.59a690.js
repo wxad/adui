d835874f8f476ce62e2a3c1e63f920a628a10d65
jiangyijie27 <jiangyijie27@users.noreply.github.com>
2023-06-20 06:58:07 +0000

    deploy: 0cabaae869b139c63126aa3497fefaca86e660eb

---
+++ components/drawer/index.md.59a690.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{1322:function(e,n){e.exports={content:["article",["p","浮层覆盖在页面内容上，并固定在屏幕的边缘。"],["h2","Props"],["h3","Drawer"]],meta:{category:"Components",type:"General",title:"Drawer",subtitle:"浮层",filename:"components/drawer/index.md"}}}}]);
