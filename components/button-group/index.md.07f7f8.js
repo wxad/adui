151679f0cfa7e285f353a18bfa2f55f6b8704ce3
jiangyijie27 <jiangyijie27@users.noreply.github.com>
2024-09-13 08:40:52 +0000

    deploy: f5081415196e58f645a4fa242ed749b920f80cfe

---
+++ components/button-group/index.md.07f7f8.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{1338:function(t,o){t.exports={content:["article",["p","按钮组提供给用户在多个选项，用于承载类似内容或功能的切换。常见于某功能的切换操作。与导航页签 Tabs 不同，按钮组不用于页面高层级的切换操作，而用于页面的辅助切换操作。"],["h2","Props"],["h3","Button.Group"]],meta:{category:"Components",type:"General",title:"ButtonGroup",subtitle:"按钮组",filename:"components/button-group/index.md"}}}}]);
---
+++ components/cascader/index.md.b91239.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{1342:function(e,n){e.exports={content:["article",["p","级联选择器直观展示已有的层级关系。"],["h2","Props"],["h3","Cascader"]],meta:{category:"Components",type:"General",title:"Cascader",subtitle:"级联选择器",filename:"components/cascader/index.md"}}}}]);
